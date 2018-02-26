02b302ba12e815f92494d641c5c9a5043ec8e57a
IrieY56 <IrieY56@gmail.com>
2018-02-25 19:54:56 -0500

    setup styling for home buttons

--- homecookd/src/Scenes/Home/HomeJoinButtons.js
+++ homecookd/src/Scenes/Home/HomeJoinButtons.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import './HomeJoinButtons.css'
 
 class HomeJoinButtons extends Component{
   render(){
     return(
       <div>
-      <RaisedButton href="/Login" label="Login" primary={true}  />
-      <RaisedButton href="/Register" label="Get Started" primary={true}  />
+        <RaisedButton href="/Login" label="Login" primary={true}  />
+        <RaisedButton href="/Register" label="Get Started" primary={true}  />
       </div>
     )
   }
