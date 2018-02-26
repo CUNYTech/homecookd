a9fb324392731883c5b0c090a4e436d663db2e24
Eric Chiu <iriey56@gmail.com>
2018-02-26 00:36:53 +0000

    Fixed hasValue state not being updated properly

--- homecookd/src/Scenes/Account/Register/RegisterForm.js
+++ homecookd/src/Scenes/Account/Register/RegisterForm.js
@@ -7,14 +7,21 @@ import './RegisterForm.css'
 class RegisterForm extends Component{
     constructor(props){
         super(props);
-        this.state = {value: ''};
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: ''
+        };
 
         this.handleFormChange = this.handleFormChange.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
 
     handleFormChange(e){
-        this.setState({value: e.target.value})
+        const value = e.target.value;
+        const name = e.target.name;
+        this.setState({[name]: value})
     }
     handleFormSubmit(e){
         e.preventDefault();
@@ -24,8 +31,8 @@ class RegisterForm extends Component{
         return(
           // <h2>Register</h2>
           <Paper className="container" onSubmit={this.handleFormSubmit}>
-                <TextField floatingLabelText="First Name" name="name[first]" type="text" value={this.state.lastName} onChange={this.handleFormChange} />
-                <TextField floatingLabelText="Last Name" name="name[last]" type="text" value={this.state.lastName} onChange={this.handleFormChange} />
+                <TextField floatingLabelText="First Name" name="firstName" type="text" value={this.state.firstName} onChange={this.handleFormChange} />
+                <TextField floatingLabelText="Last Name" name="lastName" type="text" value={this.state.lastName} onChange={this.handleFormChange} />
                 <TextField floatingLabelText="Email" name="email" type="test" value={this.state.email} onChange={this.handleFormChange} />
                 <TextField floatingLabelText="Username" name="username" type="text" value={this.state.username} onChange={this.handleFormChange} />
                 <TextField floatingLabelText="Password" name="password" type="password" value={this.state.password} onChange={this.handleFormChange} />
