0269e3777ec9a5d5efa19d67b455f150132ba9bc
khelif96 <khelif96@gmail.com>
2018-02-27 16:43:46 -0500

    Fixed error with userSchema and simplified search of existing accounts for registerUser

--- backend/Controller/auth.js
+++ backend/Controller/auth.js
@@ -6,18 +6,23 @@ var hat = require("hat");
 
 const saltRounds = 10; // how many times to salt the password
 
-// 
+//
 var rack = hat.rack(64, 16);
 
 exports.registerUser = (req, res) => {
-    if (req.body.email === undefined || req.body.password === undefined || req.body.name === undefined || req.body.userName === undefined){
+    if (req.body.email === undefined || req.body.password === undefined || req.body.name.first === undefined|| req.body.name.last === undefined || req.body.userName === undefined){
         res.status(400).json( {error: "Incomplete request"} );
         console.log("Incomplete request");
     }else{
-        User.find( {email: {$regex : new RegExp("^" + req.body.email + "$" + "i")}},function (err, docs){
+        User.find( {$or: [{email: {$regex : new RegExp(req.body.email,"i")}},
+        {userName: {$regex : new RegExp(req.body.userName,"i")}}]}
+        ,
+        function (err, docs){
+          console.log(docs.length);
+            if(err){
+              console.log("ERROR " + err);
+            }
             if(!docs.length){
-                User.find( {userName: {$regex : new RegExp("^" + req.body.userName + "$" + "i")}}, function(err, docs){
-                    if (!docs.length){
                         var tempUser = new User();
                         tempUser.name = req.body.name;
                         tempUser.email = req.body.email;
@@ -30,20 +35,15 @@ exports.registerUser = (req, res) => {
                                     console.log("Error while saving to database ");
                                     res.send(err);
                                 }
-
-                                res.status(201);
-                                res.json( {message: "Sucesfully registered", api_token: tempUser.api_token} );
+                              res.status(201);
+                              res.json( {message: "Sucesfully registered", api_token: tempUser.api_token} );
                             });
                         });
                     }else{
                         res.status(400);
                         res.json( {message: "Username belongs to another user"} );
                     }
-                }); 
-            }else{
-                res.status(400);
-                res.json( {message: "Email belongs to antoher user"} );
-            }
-        });
+                });
+
+        }
     }
-}
