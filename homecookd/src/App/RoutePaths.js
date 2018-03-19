4cd48c52548cb688f867d046369e6c23ecc9d48c
IrieY56 <IrieY56@gmail.com>
2018-03-19 12:17:48 -0400

    removed duplicate imports

--- homecookd/src/App/RoutePaths.js
+++ homecookd/src/App/RoutePaths.js
@@ -8,8 +8,6 @@ import LoginForm from '../Scenes/Account/Login/LoginForm';
 import RegisterSeller from '../Scenes/Account/Register/RegisterSeller';
 import LoginSellerForm from '../Scenes/Account/Login/LoginSellerForm';
 import RegisterForm from '../Scenes/Account/Register/RegisterForm';
-import RegisterSeller from '../Scenes/Account/Register/RegisterSeller';
-import LoginSellerForm from '../Scenes/Account/Login/LoginSellerForm';
 import AccountPage from '../Scenes/Account/MyAccount/AccountPage';
 import StorePage from '../Scenes/Store/StorePage';
 
