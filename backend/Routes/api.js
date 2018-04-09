ff4a0cef66047b3cf1c4813235cf559f171d0be5
khelif96 <khelif96@gmail.com>
2018-04-09 18:52:04 -0400

    Fixed dynamic store pages

--- backend/Routes/api.js
+++ backend/Routes/api.js
@@ -44,6 +44,9 @@ router.get('/food/foodID/:FoodID', getFoodItems.getFoodItemByID); // Returns a F
 router.get('/food/sellerID/:SellerID', getFoodItems.getFoodItemsBySellerID); // Returns an array of Food Objects
 router.post('/food/api_token',getFoodItems.getFoodItemsByAPItoken);
 
+// Get Seller information
+router.get('/seller/sellerID/:sellerID', seller.sellerInfoBySellerID);
+
 // Update Seller
 router.post('/seller/foodUpdate/:foodID', seller.updateFoodItem);
 // 404 paths
