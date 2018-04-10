c6d94427bbd0342606f3f440fe86a5b56fafdc84
galicea17 <galicea117@users.noreply.github.com>
2018-04-09 21:01:41 -0400

    added search bar

---
+++ homecookd/src/Scenes/SearchInput/SearchBar.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { Input } from 'semantic-ui-react'
+
+const style = {
+  height: '50px',
+  width: '500px',
+  margin: '10px auto'
+}
+
+export default class SearchBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchTerm: ''
+    }
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.handlekeyChange = this.handlekeyChange.bind(this);
+  }
+
+  handleSearchChange(event){
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({searchTerm: value})
+  }
+
+  handlekeyChange(event){
+    if(event.charCode === 13){
+      console.log(this.state.searchTerm); //send searchTerm to backend
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Input onChange={this.handleSearchChange} onKeyPress={this.handlekeyChange} style={style}  icon='search' placeholder='Search...' />
+      </div>
+
+    )
+  }
+}
