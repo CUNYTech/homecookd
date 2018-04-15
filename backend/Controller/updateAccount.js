0a6810f7dc30617e1ddc524b1d48ae6b4c1d1f06
mixemer <mehmetshin96@gmail.com>
2018-04-15 16:35:28 -0400

    changing the json old format to new format

--- backend/Controller/updateAccount.js
+++ backend/Controller/updateAccount.js
@@ -8,24 +8,23 @@ var passwordRegex = new RegExp("^(?=.{4,})");
 
 exports.updateSellerAccountPassword = (req, res) => {
   if(req.body.api_token === undefined || req.body.api_token === "" ){ //add if password is not filled
-      res.status(401).json( {error: "Missing api_token in request"} );
+      res.status(401).json( {success:false, error: "Missing api_token in request"} );
   }
   Seller.findOne( {api_token: req.body.api_token}, function(err, seller){
     if (!seller  || err){
-      res.status(401).json( {error: "Could not find seller with this api token"} );
+      res.status(401).json( {success:false, error: "Could not find seller with this api token"} );
     }else {
       if(req.body.oldPassword === req.body.newPassword){
-        res.status(400).json({error: "New password is the same as old one"} );
+        res.status(401).json( {success:false, error: "New password is the same as old one"} );        
       }else if(!passwordRegex.test(req.body.password)){
-        res.status(400);
-        res.json( {error: "invalid password, has to contain at least one lower, one upper case character and has to be at least 6 digits"} );
+        res.status(401).json( {success:false, error: "invalid password, has to contain at least one lower, one upper case character and has to be at least 6 digits"} );                
       }else{
         console.log("Comparing passwords");
         bcrypt.compare(req.body.oldPassword, seller.password_hash, function(err, valid){
           if (valid){
             updateSellerPassword(seller._id, req.body.newPassword, res);
           }else{
-            res.status(401).json( {error: "Invalid password"} );
+            res.status(401).json( {success:false, error: "Invalid password"} );                    
           }
         });
       }
@@ -38,7 +37,7 @@ function updateSellerPassword(seller_id, newPassword, res)
   Seller.findOne({ _id: seller_id },
     function (err, seller) {
       if (err) {
-        res.status(400).json("contact addMsg error: " + err);
+        res.status(500).json( {success:false, error: "contact addMsg error: " + err} );                            
       }else{
         bcrypt.hash(newPassword, saltRounds, function(err, hash){
           seller.password_hash = hash;
@@ -47,8 +46,7 @@ function updateSellerPassword(seller_id, newPassword, res)
               console.log("Error while saving to database ");
               res.status(500).send(err);
             }
-            res.status(201);
-            res.json( {message: "Sucesfully changed password", user_type: "Seller" ,api_token: seller.api_token} );
+            res.json( {success: true, message: "Sucesfully changed password",data:{api_token: seller.api_token, user_type: "Seller"}} );            
           });
         });
       } //else close
