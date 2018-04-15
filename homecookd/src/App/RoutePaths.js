66d5eb94649aa3dae23ba6b66968d7794ff2e821
Mohamed Khelif <khelif96@gmail.com>
2018-04-15 14:57:22 -0400

    s3 Uploads (#88)
    * Modified store page and deleted unnecessary files
    
    * Fixed Typo that prevents registering a business Name
    
    * Fixed dynamic store pages
    
    * Fixed missing import
    
    * Renamed File and created dynamic seller search page
    
    * Get a signed request route

--- homecookd/src/App/RoutePaths.js
+++ homecookd/src/App/RoutePaths.js
@@ -16,7 +16,7 @@ import AccountPage from '../Scenes/Account/MyAccount/AccountPage';
 import StorePage from '../Scenes/Store/StorePage';
 import SellerAccount from '../Scenes/Seller/SellerAccount/SellerAccount';
 
-import SellerList from '../Scenes/Seller/SellerAccount/SellerList';
+import SellerList from '../Scenes/Seller/SellerSearch/SellerList';
 
 // ADMIN Imports
 
