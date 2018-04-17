22fe5502d8168383ea092649f29199a5be7b0738
khelif96 <khelif96@gmail.com>
2018-04-16 21:17:51 -0400

    Began working on homepage design

--- homecookd/src/Scenes/Home/HomePage.js
+++ homecookd/src/Scenes/Home/HomePage.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import HomeJoinButtons from './HomeJoinButtons';
-
+import FoodChoiceGrid from './FoodChoices/FoodChoiceGrid';
 
 class HomePage extends Component{
   render(){
     return(
-      <div>
-          <center><h1>Homecookd</h1></center>
+      <div style={{backgroundColor:"#db2828", height:"100%"}}>
+          <h1>Homecookd</h1>
 
           <HomeJoinButtons/>
+          <FoodChoiceGrid/>
 
       </div>
     )
