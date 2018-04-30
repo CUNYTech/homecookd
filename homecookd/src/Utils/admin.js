1da19713f76b93e311b2c5ae0b14bd922d868e5b
IrieY56 <IrieY56@gmail.com>
2018-04-30 19:11:03 -0400

    moved base URL to a new file

--- homecookd/src/Utils/admin.js
+++ homecookd/src/Utils/admin.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
-
-var baseUrlDomain = 'http://localhost'
-const baseUrl = baseUrlDomain +":3001";
-
+import serverAddress from './serverAddress';
 
   export {loginAdmin};
     function loginAdmin(email,password){
-      return axios.post(baseUrl + "/sudoApi/auth/admin/login", {
+      return axios.post(serverAddress + "/sudoApi/auth/admin/login", {
         email: email,
         password: password
       })
@@ -15,28 +12,28 @@ const baseUrl = baseUrlDomain +":3001";
       // .catch(error => error);
   export {getUsers};
     function getUsers(api_token){
-      return axios.post(baseUrl + "/sudoApi/admin/getUsers", {
+      return axios.post(serverAddress + "/sudoApi/admin/getUsers", {
         api_token: api_token
       })
       }
   export {getSellers};
     function getSellers(api_token){
-      return axios.post(baseUrl + "/sudoApi/admin/getSellers", {
+      return axios.post(serverAddress + "/sudoApi/admin/getSellers", {
         api_token: api_token
       })
       }
 
   export{approveSeller};
     function approveSeller(api_token,seller_id){
-      return axios.post(baseUrl + "/sudoApi/admin/approveSeller",{
+      return axios.post(serverAddress + "/sudoApi/admin/approveSeller",{
         api_token: api_token,
         seller_id: seller_id
       })
     }
-    
+
     export{rejectSeller};
       function rejectSeller(api_token,seller_id){
-        return axios.post(baseUrl + "/sudoApi/admin/rejectSeller",{
+        return axios.post(serverAddress + "/sudoApi/admin/rejectSeller",{
           api_token: api_token,
           seller_id: seller_id
         })
--- homecookd/src/Utils/upload.js
+++ homecookd/src/Utils/upload.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
-
-var baseUrlDomain = 'http://localhost'
-const baseUrl = baseUrlDomain +":3001";
+import serverAddress from './serverAddress';
 
 export{getSignedUrl}
   function getSignedUrl(fileName, fileType,type){
-    return axios.post(baseUrl + "/api/sign_s3/",{
+    return axios.post(serverAddress + "/api/sign_s3/",{
       fileName: fileName,
       fileType: fileType,
       type : type
